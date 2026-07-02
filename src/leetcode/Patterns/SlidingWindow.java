11d35d9cbaa40e1c290257b5700ce0b6a7633880
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-02 15:11:09 +0530

    sliding window unserstanding

--- src/leetcode/Patterns/SlidingWindow.java
+++ src/leetcode/Patterns/SlidingWindow.java
@@ -1,5 +1,24 @@
 package leetcode.Patterns;
 
 public class SlidingWindow {
-
+    static int max_sum(int[] arr, int window) {
+        int current = 0;
+        for (int i = 0; i < window; i++) {
+            current+=arr[i];
+        }
+        int max = current;
+        int n = arr.length;
+        for (int j = 1; j <= n-window; j++) {
+            current = current - arr[j-1] + arr[j+window-1];
+            if (current > max) {
+                max = current;
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] arr = {3,8,2,5,7,6,12};
+        int window = 4;
+        System.out.println(max_sum(arr, window));
+    }
 }
---
+++ src/leetcode/p20/Solution.java
@@ -0,0 +1,47 @@
+package leetcode.p20;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+
+public class Solution {
+    static public boolean isValid(String s) {
+        Stack<Integer> stack = new Stack<>();
+        boolean ans = false;
+        HashMap<Character, Integer> hashmap = new HashMap<>();
+        hashmap.put('[',-2);
+        hashmap.put(']',2);
+        hashmap.put('{',-1);
+        hashmap.put('}',1);
+        hashmap.put('(',-3);
+        hashmap.put(')',3);
+        for (int i = 0; i < s.length(); i++) {
+            char word = s.charAt(i);
+            if (hashmap.containsKey(word)) {
+                int val = hashmap.get(word);
+                if (val < 0) {
+                    stack.push(val);
+                }
+                else {
+                    if (stack.isEmpty()) {
+                        return false;
+                    }
+                    if (stack.peek() + val == 0) {
+                        stack.pop();
+                    } else {
+                        return false;
+                    }
+                }
+            }
+            System.out.println("Contains");
+        }
+        System.out.println(hashmap);
+        System.out.println(stack);
+        return stack.isEmpty();
+    }
+    public static void main(String[] args) {
+        String s = "[]{}(";
+        System.out.println(isValid(s));
+    }
+}
