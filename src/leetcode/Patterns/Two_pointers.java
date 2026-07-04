ea3258ea29026cdf8cbb7274effc6c8d1708fdcb
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-04 19:36:09 +0530

    solving leetcode 1, 2390 and other for 2 pointer

---
+++ src/leetcode/Patterns/Two_pointers.java
@@ -0,0 +1,31 @@
+package leetcode.Patterns;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Two_pointers {
+    static public List<Integer> two_pointer(int[] arr, int val) {
+        List<Integer> list = new ArrayList<>();
+        int left = 0;
+        int right = arr.length-1;
+        while (left < right) {
+            int current_sum = arr[left]+arr[right];
+            if (current_sum == val) {
+                list.add(arr[left]);
+                list.add(arr[right]);
+                break;
+            }
+            else if (current_sum < val ) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return list;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1,23,42,13,78,230};
+        System.out.println(two_pointer(arr, 308));
+    }
+}
---
+++ src/leetcode/p1/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.p1;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class Solution {
+    static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (hashMap.containsKey(complement)) {
+                return new int[]{hashMap.get(complement), i};
+            }
+            hashMap.put(nums[i], i);
+        }
+        return new int[]{-1,-1};
+    }
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        System.out.println(twoSum(nums, target));
+    }
+}
