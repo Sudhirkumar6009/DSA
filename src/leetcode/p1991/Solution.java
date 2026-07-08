883a9db2d48254791810d26e781eb7bf245769c3
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-08 23:19:38 +0530

    prefix sum easy leetcode solved

--- src/leetcode/p1991/Solution.java
+++ src/leetcode/p1991/Solution.java
@@ -1,10 +1,20 @@
 package leetcode.p1991;
 
+import java.util.Arrays;
+
 public class Solution {
     static public int findMiddleIndex(int[] nums) {
-        int res = 0;
-
-        return res;
+        int[] leftPrefixSum = new int[(nums.length)+1];
+        leftPrefixSum[0] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            leftPrefixSum[i+1] = leftPrefixSum[i] + nums[i];
+        }
+        for (int j = 0; j < nums.length ; j++) {
+            int left = leftPrefixSum[j];
+            int right = leftPrefixSum[nums.length] - leftPrefixSum[j+1];
+            if (left == right) return j;
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
