ab7b25b61f9d9a1ddf1e039a1abfed61f8c000b2
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-08 22:51:13 +0530

    new leetcode problem is solved

--- src/leetcode/p1732/Solution.java
+++ src/leetcode/p1732/Solution.java
@@ -2,14 +2,18 @@ package leetcode.p1732;
 
 public class Solution {
     static public int largestAltitude (int[] gain) {
-        int[] prefixSum = new int[gain.length-1];
+        int[] prefixSum = new int[(gain.length)+1];
         int max = prefixSum[0];
-
+        prefixSum[0] = max;
+        for (int i = 0; i < gain.length; i++) {
+            prefixSum[i+1] = prefixSum[i] + gain[i];
+            if (max < prefixSum[i+1]) max = prefixSum[i+1];
+        }
         return max;
     }
 
     public static void main(String[] args) {
-        int[] gain = {-5,1,5,0,-7};
+        int[] gain = {-4,-3,-2,-1,4,3,2};
         System.out.println(largestAltitude(gain));
     }
 }
