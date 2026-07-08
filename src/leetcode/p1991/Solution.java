d3ed2fef38fe5e6f70ccf347c58a654fc20b9607
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-08 22:55:34 +0530

    solving leetcode 1991

---
+++ src/leetcode/p1991/Solution.java
@@ -0,0 +1,14 @@
+package leetcode.p1991;
+
+public class Solution {
+    static public int findMiddleIndex(int[] nums) {
+        int res = 0;
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,-1,8,4};
+        System.out.println(findMiddleIndex(nums));
+    }
+}
