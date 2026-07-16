058f22c093bf375006b780e068647187d3af8d04
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-16 19:33:41 +0530

    leetcode 977 solved

--- src/leetcode/p977/Solution.java
+++ src/leetcode/p977/Solution.java
@@ -1,4 +1,20 @@
 package leetcode.p977;
 
+import java.util.Arrays;
+
 public class Solution {
+    public static int[] sortedSquare(int[] nums) {
+        Arrays.sort(nums);
+        int[] arr = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = nums[i]*nums[i];
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-4,-1,0,-3,10};
+        System.out.println(Arrays.toString(sortedSquare(nums)));
+    }
 }
