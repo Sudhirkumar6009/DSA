eb22ccf56520a0bfd98e939aae6764f4ab2d60ca
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-17 19:13:37 +0530

    leetcode 11 solved

--- src/leetcode/p11/Solution.java
+++ src/leetcode/p11/Solution.java
@@ -1,27 +1,20 @@
 package leetcode.p11;
 
-import java.util.Arrays;
-import java.util.HashSet;
-
 public class Solution {
     public static int maxArea(int[] height) {
         int res = 0;
-        HashSet<Integer> hashSet = new HashSet<>();
-        int[][] arr = new int[height.length][height.length];
         for (int i = 0; i < height.length; i++) {
-            for (int j = 1; j < height.length; j++) {
-                int min = Math.min(height[i], height[j]);
-                arr[i][j] = min*j;
+            for (int j = i+1; j < height.length; j++) {
+                int area = Math.min(height[i], height[j])*(j-i);
+                res = Math.max(res,area);
             }
         }
-        for (int[] num: arr){
-            System.out.println(Arrays.toString(num));
-        }
+
         return res;
     }
 
     public static void main(String[] args) {
-        int[] height = {1,8,6,2,5,4,8,3,7};
+        int[] height = {12,23,34,45,56};
         System.out.println(maxArea(height));
     }
 }
