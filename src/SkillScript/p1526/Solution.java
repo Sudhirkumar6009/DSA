83197f9fd31e86da0bf54fd6c2be445e24df96cc
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-17 19:58:38 +0530

    skillscript 1526 solved

---
+++ src/SkillScript/p1526/Solution.java
@@ -0,0 +1,29 @@
+package SkillScript.p1526;
+
+import java.util.HashSet;
+
+public class Solution {
+    public static String recoverString(String s, String k) {
+        HashSet<Character> stuck = new HashSet<>();
+        for (char ch: k.toCharArray()) {
+            stuck.add(ch);
+        }
+        StringBuilder res = new StringBuilder();
+        for (char c: s.toCharArray()) {
+            if (!stuck.contains(c)) {
+                res.append(c);
+            } else {
+                if (res.length() == 0 || res.charAt(res.length() - 1) != c) {
+                    res.append(c);
+                }
+            }
+        }
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        String s = "helllloooowoorrlddd";
+        String k = "lo";
+        System.out.println(recoverString(s,k));
+    }
+}
