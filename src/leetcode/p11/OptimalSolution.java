bb430b99890b5677b28984d28fa62bcff3b81c6e
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-17 19:34:10 +0530

    leetcode 11 solved with minimal and most optimal code solution through two pointers approach

---
+++ src/leetcode/p11/OptimalSolution.java
@@ -0,0 +1,26 @@
+package leetcode.p11;
+
+public class OptimalSolution {
+    public static int maxArea(int[] height) {
+        int res = 0;
+        int left = 0;
+        int right = height.length-1;
+        while (left < right) {
+            int  width = right - left;
+            int ans = width * Math.min(height[left], height[right]);
+            res = Math.max(res, ans);
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(height));
+    }
+}
+
