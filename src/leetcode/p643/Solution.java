9c419816d1debab465fd73a8f4eb48f4b7fd8882
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-19 23:28:57 +0530

    leetcode 643 solved

---
+++ src/leetcode/p643/Solution.java
@@ -0,0 +1,26 @@
+package leetcode.p643;
+
+public class Solution {
+    public static double findMaxAverage(int[] nums, int k) {
+        double sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+        double max = sum;
+
+        for (int i = k ; i < nums.length; i++) {
+            sum += nums[i];
+            sum -= nums[i-k];
+            if (sum > max) {
+                max = sum;
+            }
+        }
+        return max/k;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,12,-5,-6,50,3};
+        int k = 4;
+        System.out.println(findMaxAverage(nums,k));
+    }
+}
