a1ee7425ba460ae600fe33202010c69df35441c5
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-22 19:15:19 +0530

    solving new sliding windows leetcode

---
+++ src/leetcode/p1876/Solution.java
@@ -0,0 +1,13 @@
+package leetcode.p1876;
+
+public class Solution {
+    public static int countGoodSubstrings(String s) {
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String s = "xyzzaz";
+        System.out.println(countGoodSubstrings(s));
+    }
+}
