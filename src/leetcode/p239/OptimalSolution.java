1dc384d7caf28aecc41676bc40e1dd88165bcd91
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-26 18:36:51 +0530

    trying to solve leetcode 239 hard one with optimal method through queue + sliding window

---
+++ src/leetcode/p239/OptimalSolution.java
@@ -0,0 +1,16 @@
+package leetcode.p239;
+
+import java.util.Arrays;
+
+public class OptimalSolution {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+     int[] nums = {1,3,-1,-3,5,3,6,7};
+     int k = 3;
+        System.out.println(Arrays.toString(maxSlidingWindow(nums,k)));
+    }
+}
