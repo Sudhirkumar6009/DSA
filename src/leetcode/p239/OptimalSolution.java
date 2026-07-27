21d05bb38bfebcbfb13faafde0e7a6228adc2ce7
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-07-27 18:21:47 +0530

    Solved hard leetcode p239

--- src/leetcode/p239/OptimalSolution.java
+++ src/leetcode/p239/OptimalSolution.java
@@ -1,11 +1,32 @@
 package leetcode.p239;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Deque;
 
 public class OptimalSolution {
     public static int[] maxSlidingWindow(int[] nums, int k) {
+        int[] ans = new int[nums.length-k+1];
+        Deque<Integer> deque = new ArrayDeque<>();
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+        }
+        ans[0] = nums[deque.peekFirst()];
+        for (int i = k; i < nums.length; i++) {
+            if (deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            while (!deque.isEmpty() && nums[deque.peekLast()] <= nums[i]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
 
-        return new int[] {};
+            ans[i - k + 1] = nums[deque.peekFirst()];
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
