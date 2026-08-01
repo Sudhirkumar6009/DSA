ec9d67ebc37f23c636c852371de074b1ece6af2b
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-08-01 20:11:48 +0530

    Leetcode 19 solved

--- src/leetcode/p19/Solution.java
+++ src/leetcode/p19/Solution.java
@@ -10,15 +10,24 @@ public class Solution {
         }
     }
     public static ListNode removeNthFromEnd(ListNode head, int n) {
-        if (head.next == null) return head;
-        ListNode slow = head;
-        ListNode fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
         }
-//        condition for removing Nth node after slow pointer
-        return head;
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
     }
 
     public static void main(String[] args) {
