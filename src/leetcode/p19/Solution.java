a9b8eeb98636867c62c7cc86791706fe03b3cae1
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-08-01 20:12:26 +0530

    leetcode 19 solved properly

--- src/leetcode/p19/Solution.java
+++ src/leetcode/p19/Solution.java
@@ -12,21 +12,16 @@ public class Solution {
     public static ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-
         ListNode slow = dummy;
         ListNode fast = dummy;
-
         for (int i = 0; i <= n; i++) {
             fast = fast.next;
         }
-
         while (fast != null) {
             slow = slow.next;
             fast = fast.next;
         }
-
         slow.next = slow.next.next;
-
         return dummy.next;
     }
 
