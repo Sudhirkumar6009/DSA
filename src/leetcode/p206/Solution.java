7e8ca9a1fc7da67cc1935ec8e1d748bbc966f2f9
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-08-02 18:47:05 +0530

    Leetcode 206 soled

---
+++ src/leetcode/p206/Solution.java
@@ -0,0 +1,40 @@
+package leetcode.p206;
+
+public class Solution {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public static ListNode reverseList (ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+
+        System.out.println(reverseList(node1).val);
+        
+        
+    }
+}
