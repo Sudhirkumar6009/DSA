b9e9676f87e7e06f0a2bbebd0b92ae586681f31b
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-08-05 23:02:11 +0530

    Solving leetcode p83

--- src/leetcode/p83/Solution.java
+++ src/leetcode/p83/Solution.java
@@ -1,4 +1,44 @@
 package leetcode.p83;
 
-public class Solutino {
+public class Solution {
+    static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+    public static ListNode deleteDuplicates (ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            if (curr.val == next.val) {
+                curr.next = new ListNode(0);
+                prev.next = next;
+                curr = next;
+            } else {
+                prev = curr;
+                curr = next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(1);
+        ListNode n3 = new ListNode(2);
+        ListNode n4 = new ListNode(3);
+        ListNode n5 = new ListNode(3);
+
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n5;
+
+        System.out.println(deleteDuplicates(n1).val);
+
+    }
 }
