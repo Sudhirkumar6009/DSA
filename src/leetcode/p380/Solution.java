d46c9f06b6a03501ce3eb012215cd2cb9fad0d40
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-08-11 18:08:49 +0530

    Leetcode 380 solved First Design Question

--- src/leetcode/p380/Solution.java
+++ src/leetcode/p380/Solution.java
@@ -2,16 +2,22 @@ package leetcode.p380;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Random;
 
 public class Solution {
-    private static ArrayList<Integer> list;
-    private static HashMap<Integer, Integer> map;
 
-    public Solution () {
+    private ArrayList<Integer> list;
+    private HashMap<Integer, Integer> map;
+    private Random random;
+
+    public Solution() {
         list = new ArrayList<>();
         map = new HashMap<>();
+        random = new Random();
     }
-    public static boolean insert (int val) {
+
+    public boolean insert(int val) {
+
         if (map.containsKey(val)) {
             return false;
         }
@@ -19,26 +25,36 @@ public class Solution {
         map.put(val, list.size() - 1);
         return true;
     }
-    public static boolean remove (int val) {
+
+    public boolean remove(int val) {
         if (!map.containsKey(val)) {
             return false;
         }
-
         int index = map.get(val);
-
         int lastValue = list.get(list.size() - 1);
-
         list.set(index, lastValue);
-
         map.put(lastValue, index);
-
         list.remove(list.size() - 1);
-
         map.remove(val);
-
         return true;
     }
 
+    public int getRandom() {
+        int index = random.nextInt(list.size());
+        return list.get(index);
+    }
+
     public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        System.out.println(obj.insert(10));      // true
+        System.out.println(obj.insert(20));      // true
+        System.out.println(obj.insert(10));      // false
+
+        System.out.println(obj.remove(10));      // true
+        System.out.println(obj.remove(30));      // false
+
+        System.out.println(obj.getRandom());    // 20
     }
 }
