6892675da164c93e89a490fb8581e5b68a1c5a79
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-08-27 20:03:40 +0530

    Solving Leetcode 496

---
+++ src/leetcode/p496/Solution.java
@@ -0,0 +1,15 @@
+package leetcode.p496;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static int[] nextGreaterElement (int[] nums1, int[] nums2) {
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {4,1,2};
+        int[] nums2 = {1,3,4,2};
+        System.out.println(Arrays.toString(nextGreaterElement(nums1, nums2)));
+    }
+}
