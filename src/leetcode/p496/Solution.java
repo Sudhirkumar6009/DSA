9ce8beae356f836a53b7c0b7ea0178e223b81957
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-08-27 23:09:34 +0530

    Leetcode 496 solved

--- src/leetcode/p496/Solution.java
+++ src/leetcode/p496/Solution.java
@@ -1,15 +1,33 @@
 package leetcode.p496;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Stack;
 
-public class Solution {
-    public static int[] nextGreaterElement (int[] nums1, int[] nums2) {
-        return new int[] {};
+class Solution {
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        int[] ans = new int[nums1.length];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(nums2[0]);
+        for (int i = 1; i < nums2.length; i++) {
+            while (!stack.isEmpty() && stack.peek() < nums2[i]) {
+                hashMap.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+        for (int num : stack) hashMap.put(num,-1);
+        for (int i = 0; i < nums1.length; i++) {
+            if (hashMap.containsKey(nums1[i])) {
+                ans[i] = hashMap.get(nums1[i]);
+            }
+        }
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {4,1,2};
-        int[] nums2 = {1,3,4,2};
+        int[] nums1 = {1,3,5,2,4};
+        int[] nums2 = {6,5,4,3,2,1,7};
         System.out.println(Arrays.toString(nextGreaterElement(nums1, nums2)));
     }
 }
