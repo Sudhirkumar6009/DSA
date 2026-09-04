d6f41f49823846b2031bdb8347b4e043456f1079
Sudhirkumar6009 <sudhir.kuchara@gmail.com>
2026-09-04 22:59:24 +0530

    Leetcode daily p3903 solved

---
+++ src/leetcode/p3903/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.p3903;
+
+public class Solution {
+    public static int firstStableIndex(int[] nums, int k) {
+        int[] maxArr = new int[nums.length];
+        int[] minArr = new int[nums.length];
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            max = Math.max(max,nums[i]);
+            maxArr[i] = max;
+        }
+        for (int i = nums.length-1; i >= 0; i--) {
+            min = Math.min(min,nums[i]);
+            minArr[i] = min;
+        }
+        for (int read = 0; read < nums.length; read++) {
+            if ((maxArr[read]-minArr[read]) <= k) {
+                return read;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = {5,0,1,4};
+        int k = 3;
+        System.out.println(firstStableIndex(nums, k));
+    }
+}
